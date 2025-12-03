76b524c29bf30c7ad5f2d0a63d6e27caa236da70
davidcargosegura-oss <david.cargosegura@gmail.com>
2025-12-03 18:34:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,6 @@ admin.add_view(ProtectedAdminView(User, db.session, name='Usuarios'))
 
 # --- 5. LÓGICA DE INICIO DE SESIÓN Y USUARIO INICIAL ---
 
-@app.cli.before_app_serving
 def create_db_and_admin():
     """Crea la base de datos y un usuario administrador inicial si no existen."""
     try:
